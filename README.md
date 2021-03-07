b4722f5bc90028a66e7330d491ad995017e31e12
Claire Davies <claire.davies@junitech.com>
2021-03-07 10:44:48 +0000

    first draft of readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# note-taker
+# Note Taker  ![Github license](https://img.shields.io/github/license/ClaireMDavies/readme-generator)
+ 
+## Description
+Having been given the front end code for a note taking app, the task was to create the back end functionality, creating the routes on a server to allow the page to function as expected.   
+
+## Table of Contents
+  - [Note Taker](#note-taker)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Technologies Used](#technologies-used)
+  - [Future versions](#future-versions)
+
+## Installation
+Initiate the app by opening the terminal, and running npm install, followed by node server.js, then navigating to the browser at locl host:3000. Or accessing at heroku.
+
+## Usage
+After navigating from the home page to the notes page, the user can add notes, save them, and delete them.
+
+A screenshot of the rendered html page is shown below.
+![note-taker.png](note-taker.png)
+
+## License
+License with MIT
+
+## Contributors
+This app is the sole work of [Claire](https://github.com/ClaireMDavies)
+
+## Technologies Used
+- Javascript
+- node.js
+- express npm
+- path npm
+- fs npm
+- uuid v4 npm
+  
+
+## Future versions
+Given that the front code was already supplied, I was frustrated with the browser side functionality, and would make it more useable such as having the ability to go back and edit notes, along with being able to submit a note to save on the keystroke of the enter key.  
+
+
--- server.js
+++ server.js
@@ -1,75 +1,70 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const generateUniqueId = require('generate-unique-id');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 
+const databaseFile = "db/db.json";
 
 const PORT = 3000;
 
 app.use(express.static(__dirname + "/public"));
-//app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//creating the route to render the home page
+// route to render the home page
 app.get("/", (req, res) => {
+
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-//creating the route to render the notes page
+// route to render the notes page
 app.get("/notes", (req, res) => {
+    
     res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
 
-//create api route to read saved data
+// api route to read saved data
 app.get("/api/notes", (req, res) => {
 
     var notes = readNotes();
     res.send(notes);
 });
 
-//create api route to post data to db.json
-
+// api route to post data to db.json
 app.post("/api/notes", (req, res) => {
 
     const title = req.body.title;
     const text = req.body.text;
-    const id = generateUniqueId({
-        length: 5,
-    });
+    const id = uuidv4();
 
-    var newNote = { "id": id, "title": title, "text": text };
+    let newNote = { "id": id, "title": title, "text": text };
 
-    console.log(newNote);
-
-    var notes = readNotes();
+    let notes = readNotes();
     notes.push(newNote);
 
-    fs.writeFileSync('db/db.json', JSON.stringify(notes), 'utf8');
+    saveNotes(notes);
 
     res.send(notes);
 });
 
-//create route to delete data
-
+// route to delete data
 app.delete("/api/notes/:id", (req, res) => {
 
-    var notes = readNotes();
-    var deleteId = req.params.id;
+    let notes = readNotes();
+    let deleteId = req.params.id;
     
     let newArray = notes.filter(note => note.id != deleteId);
 
-    fs.writeFileSync('db/db.json', JSON.stringify(newArray), 'utf8');
+    saveNotes(newArray);
 
     res.send(newArray);
-
 });
-
 
 function readNotes() {
 
-    var fileContent = fs.readFileSync("db/db.json", "utf8");
-    var notes = [];
+    let fileContent = fs.readFileSync(databaseFile, "utf8");
+    let notes = [];
 
     if (fileContent.length > 0) {
         notes = JSON.parse(fileContent);
@@ -78,6 +73,13 @@ function readNotes() {
     return notes;
 }
 
+function saveNotes(notes) {
+
+    fs.writeFileSync(databaseFile, JSON.stringify(notes), 'utf8');
+}
+
 app.listen(PORT, function () {
+
     console.log("App listening on PORT:" + PORT);
 });
+
